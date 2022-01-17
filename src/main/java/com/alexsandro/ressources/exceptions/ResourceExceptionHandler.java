27264d7ac9cc3b47c723baea7698b2ba0b852340
Alexsandro Machado <lekomachado@hotmail.com>
2022-01-17 19:07:00 -0300

    updating error class

--- src/main/java/com/alexsandro/ressources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/alexsandro/ressources/exceptions/ResourceExceptionHandler.java
@@ -1,22 +1,61 @@
 package com.alexsandro.ressources.exceptions;
 
+import java.time.Instant;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.alexsandro.services.exceptions.DatabaseException;
+import com.alexsandro.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request ){
-	HttpStatus status = HttpStatus.BAD_REQUEST;
-	StandardError err = new StandardError();
-		return null;	
+	public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setError("Database Exception");
+		err.setMensagem(e.getMessage());
+		err.setPath(request.getRequestURI());
+		err.setStatus(status.value());
+		err.setTimestamp(Instant.now());
+
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFoundException(ResourceNotFoundException e,
+			HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setError("Resource not found");
+		err.setMensagem(e.getMessage());
+		err.setPath(request.getRequestURI());
+		err.setStatus(status.value());
+		err.setTimestamp(Instant.now());
+
+		return ResponseEntity.status(status).body(err);
 	}
 	
+
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<StandardError> MethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e,
+			HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setError("Resource not found");
+		err.setMensagem(e.getMessage());
+		err.setPath(request.getRequestURI());
+		err.setStatus(status.value());
+		err.setTimestamp(Instant.now());
+
+		return ResponseEntity.status(status).body(err);
+	}
+
 }
